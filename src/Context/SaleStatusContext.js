9e7a58973ea58cf1c829ae077139b7a7f1e7d8fd
LoLoSenPai <loic.dlugosz@gmail.com>
2023-07-25 17:09:24 +0200

    minor fix

--- src/Context/SaleStatusContext.js
+++ src/Context/SaleStatusContext.js
@@ -1,7 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { Network, Alchemy } from 'alchemy-sdk';
-import RaffleABI from '../contracts/Raffle.json';
-import { ethers } from "ethers";
 
 export const SaleStatusContext = createContext();
 
@@ -27,41 +25,36 @@ export const SaleStatusProvider = ({ children }) => {
 
     const alchemy = new Alchemy(settings);
 
-    const contractNftAddress = "0x9656E2C59D5D34479fecb112b122DB976C1FcF57"
-    const contractRaffleAddress = "0x71343cC324C9c688723c70e0d1FC328dba6E4D86";
-
-    const contractNft = new ethers.Contract(contractNftAddress, RaffleABI.abi, alchemy.config.getProvider());
-
 
     useEffect(() => {
         const interval = setInterval(async () => {
 
             const maticProvider = await alchemy.config.getProvider();
             const block = await maticProvider.getBlock();
-            const contractRaffleBeforeConnection = new ethers.Contract(contractRaffleAddress, RaffleABI.abi, maticProvider);
-            const dateStartNft = await contractNft.saleStartTime();
-            const dateEndtNftGuaranteed = await contractNft.endTimeGuaranteed();
-            const startTime = await contractRaffleBeforeConnection.startDate();
-            const deadline = await contractRaffleBeforeConnection.deadline();
+            // const contractRaffleBeforeConnection = new ethers.Contract(contractRaffleAddress, RaffleABI.abi, maticProvider);
+            // const dateStartNft = await contractNft.saleStartTime();
+            // const dateEndtNftGuaranteed = await contractNft.endTimeGuaranteed();
+            // const startTime = await contractRaffleBeforeConnection.startDate();
+            // const deadline = await contractRaffleBeforeConnection.deadline();
 
-            // const dateStartNft = 1690230600;
-            // const dateEndtNftGuaranteed = 1690290000;
-            // const startTime = 1690293600;
-            // const deadline = 1690295400;
+            const dateStartNft = 1690230600;
+            const dateEndtNftGuaranteed = 1690290000;
+            const startTime = 1690293600;
+            const deadline = 1690295400;
 
             let guaranteedStatus = '';
-            if (block.timestamp < dateStartNft.toNumber()) {
+            if (block.timestamp < dateStartNft) {
                 guaranteedStatus = "Not Started";
-            } else if (block.timestamp >= dateStartNft.toNumber() && block.timestamp <= dateEndtNftGuaranteed.toNumber()) {
+            } else if (block.timestamp >= dateStartNft && block.timestamp <= dateEndtNftGuaranteed) {
                 guaranteedStatus = "Live";
             } else {
                 guaranteedStatus = "Ended";
             }
 
             let publicSaleStatus = '';
-            if (block.timestamp < startTime.toNumber()) {
+            if (block.timestamp < startTime) {
                 publicSaleStatus = "Not Started";
-            } else if (block.timestamp >= startTime.toNumber() && block.timestamp <= deadline.toNumber()) {
+            } else if (block.timestamp >= startTime && block.timestamp <= deadline) {
                 publicSaleStatus = "Live";
             } else {
                 publicSaleStatus = "Ended";
