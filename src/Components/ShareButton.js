862a8fc8e935ce86f58ec6346d970e6dc7294333
LoLoSenPai <loic.dlugosz@gmail.com>
2023-07-31 12:53:05 +0200

    try share button

--- src/Components/ShareButton.js
+++ src/Components/ShareButton.js
@@ -3,7 +3,7 @@ import { TwitterShareButton, TwitterIcon } from 'react-share'
 export default function ShareButton() {
   return (
     <div className='ml-20'>
-        <TwitterShareButton url={'https://launchpad.ogronex.com'} title={'NFT just minted!'} via={'I just mint my NFT on this new raffle system!'} hashtags={['NFTFUNAGAIN', 'FREEMINT']} related={['ogronex', 'boxbies']}>
+        <TwitterShareButton url={'https://launchpad.ogronex.com'} title={'I just minted my NFT ! @ogronex'} via={'Ogronex'} hashtags={['NFTFUNAGAIN', 'FREEMINT']}>
             <TwitterIcon size={32} round={true} />
         </TwitterShareButton>
     </div>
