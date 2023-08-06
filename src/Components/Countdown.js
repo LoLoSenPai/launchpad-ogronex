dd2373e49f609c605cd3d001184cce9347ed68e0
LoLoSenPai <loic.dlugosz@gmail.com>
2023-08-06 16:58:27 +0200

    minor

--- src/Components/Countdown.js
+++ src/Components/Countdown.js
@@ -17,10 +17,10 @@ const CountdownComponent = ({ deadline }) => {
         } else {
             return (
                 <div className="flex">
-                    {days > 0 && <span className="w-8 md:w-6 text-center">{String(days).padStart(2, '0')}D</span>}
-                    {hours > 0 && <span className="w-8 md:w-6 text-center">{String(hours).padStart(2, '0')}H</span>}
-                    {minutes >= 0 && <span className="w-8 md:w-6 text-center">{String(minutes).padStart(2, '0')} :</span>}
-                    {seconds >= 0 && <span className="w-8 md:w-6 text-start">{String(seconds).padStart(2, '0')}</span>}
+                    {days > 0 && <span className="w-8 md:w-6 xl:w-9 text-center">{String(days).padStart(2, '0')}D</span>}
+                    {hours > 0 && <span className="w-8 md:w-6 xl:w-9 text-center">{String(hours).padStart(2, '0')}H</span>}
+                    {minutes >= 0 && <span className="w-8 md:w-6.5 xl:w-9 text-center">{String(minutes).padStart(2, '0')} :</span>}
+                    {seconds >= 0 && <span className="w-8 md:w-6 xl:w-9 text-start">{String(seconds).padStart(2, '0')}</span>}
                 </div>
             );
         }
